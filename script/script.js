e468ef0d49a948a16d445185cd5bc972da762a83
Rustam Saidgareev <rsaidgareev@gmail.com>
2020-06-06 16:28:06 -0400

    Исправление

--- script/script.js
+++ script/script.js
@@ -1,8 +1,10 @@
-var money = 1;
-var income = 2;
-var addExpenses = 3;
-var deposit = 4;
-var mission = 5;
-var period = 6;
-var str = 'Hello World';
-console.log(str);
+let money;
+let income;
+let addExpenses;
+let deposit;
+let mission;
+let period;
+let str = 'Hello World';
+    console.log(str);
+
+alert ('hi');
